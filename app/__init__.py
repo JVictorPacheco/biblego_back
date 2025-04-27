e948b97c8c6c6515d8479ef3ab8f0c367c5c10c9
João Pacheco <95194054+JVictorPacheco@users.noreply.github.com>
2025-04-27 10:50:56 -0300

    Rota de login e login protegido swagger
    Foi realizado a documentação via swagger das rotas de login e login protegido.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,27 @@
 from flask import Flask
 # from App.Routes.task_routes import task_blueprint
 from  app.Routes.user_routes import user_blueprint
+from flasgger import Swagger
+import os
 
 
 def create_app():
     app_run = Flask(__name__)
+    
+    app_run.config['SWAGGER'] = {
+        'title': 'API BibleGo',
+        'uiversion': 3,
+        'specs_route': '/api-docs/',
+        'static_url_path': '/flasgger-static',
+        'config_file': os.path.join(os.path.dirname(__file__), '..', 'swagger_config.yml')  # Caminho para o YAML
+    }
+    
+    Swagger(app_run)
     app_run.register_blueprint(user_blueprint)
     return app_run
+    
+    
+    # swagger = Swagger(app_run)
+    # app_run.register_blueprint(user_blueprint)
+    # return app_run
 
--- run.py
+++ run.py
@@ -1,12 +1,8 @@
-# import sys
-# import os
-
-# # Adiciona o diretório raiz ao PYTHONPATH
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from app import create_app
+
     
 app_run = create_app()
+
 
 if __name__ == '__main__':
     app_run.run(debug=True)
